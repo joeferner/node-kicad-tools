115a50ed3917865a960129dc2d9da18ac8c8b539
Joe Ferner <joe@fernsroth.com>
2015-08-13 14:51:17 -0400

    fix associate for new format

--- bin/kicad-associate.js
+++ bin/kicad-associate.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log("Associates schematic symbols with PCB footprints");
 console.log("TODO: Validate schematic symbol with inventory");
 
 var optimist = require('optimist');
@@ -25,63 +24,70 @@ if (args.help) {
 
 function run(callback) {
   async.auto({
-    'netFile': findNetFile,
-    'cmpFile': findCmpFile,
-    'refsToInventoryId': ['netFile', readNetFile],
-    'parts': ['refsToInventoryId', resolveParts],
-    'refToModuleName': ['cmpFile', 'parts', associate],
-    'copyKiCadLibraryModule': ['refToModuleName', copyKiCadLibraryModule]
+    'schFileNames': findSchFiles,
+    'schFiles': ['schFileNames', readSchFiles],
+    'parts': ['schFiles', resolveParts],
+    'copyKiCadLibraryModule': ['parts', copyKiCadLibraryModule],
+    'associate': ['schFiles', 'parts', associate],
+    'writeSchFiles': ['associate', writeSchFiles]
   }, callback);
 }
 
-function findFileByExt(ext, callback) {
+function findFilesByExt(ext, callback) {
   glob("*." + ext, function(err, files) {
     if (err) {
       return callback(err);
     }
-    if (files.length == 1) {
-      return callback(null, files[0]);
-    }
-    return callback(new Error("Too many ." + ext + " files found: " + files));
+    return callback(null, files);
   });
 }
 
-function findNetFile(callback) {
-  return findFileByExt('net', callback);
+function findSchFiles(callback) {
+  return findFilesByExt('sch', callback);
 }
 
-function findCmpFile(callback) {
-  return findFileByExt('cmp', callback);
-}
-
-function readNetFile(callback, data) {
-  var netFile = data.netFile;
-  return fs.readFile(netFile, 'utf8', function(err, data) {
-    if (err) {
-      return callback(err);
-    }
-    var lines = data
-      .split(/\n/)
-      .map(function(str) { return str.trim(); });
-    var results = {};
-    var ref = null;
-    for (var i = 0; i < lines.length; i++) {
-      var line = lines[i];
-      var m = line.match(/^\(comp \(ref (.*?)\)$/);
-      if (m) {
-        ref = m[1];
+function readSchFiles(callback, data) {
+  var schFileNames = data.schFileNames;
+  console.log('readSchFiles', schFileNames);
+  async.map(schFileNames, function(schFile, callback) {
+    console.log('readSchFile', schFile);
+    return fs.readFile(schFile, 'utf8', function(err, data) {
+      if (err) {
+        return callback(err);
       }
-      m = line.match(/^\(field \(name inventoryId\) (.*?)\)\)$/);
-      if (m) {
-        results[ref] = parseInt(m[1]);
+      var lines = data
+        .split(/\n/);
+      var results = {
+        schFile: schFile,
+        inventoryIds: [],
+        refToModule: {},
+        lines: lines,
+        refsToInventoryId: {}
+      };
+      var ref = null;
+      for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].trim();
+        var m = line.match(/^L (.*?) (.*?)$/);
+        if (m) {
+          ref = m[2];
+        }
+        m = line.match(/^F .*? "kicadlib:(.*?)" .*$/);
+        if (m) {
+          results.refToModule[ref] = m[1];
+        }
+        m = line.match(/^F .*? "(.*?)" .* "inventoryId"$/);
+        if (m) {
+          results.refsToInventoryId[ref] = m[1];
+          results.inventoryIds.push(m[1]);
+        }
       }
-    }
-    return callback(null, results);
-  });
+      return callback(null, results);
+    });
+  }, callback);
 }
 
 function resolveParts(callback, data) {
-  var inventoryIds = hashValuesToSet(data.refsToInventoryId);
+  var inventoryIds = toSet([].concat.apply([], data.schFiles.map(function(r) { return r.inventoryIds; })));
   var parts = {};
   return async.forEachLimit(inventoryIds, 5, function(inventoryId, callback) {
     var url = partUrlPrefix + inventoryId;
@@ -101,67 +107,10 @@ function resolveParts(callback, data) {
   });
 }
 
-function associate(callback, data) {
-  return fs.readFile(data.cmpFile, 'utf8', function(err, cmpFile) {
-    if (err) {
-      return callback(err);
-    }
-    var refToModuleName = {};
-    var lines = cmpFile
-      .split(/\n/)
-      .map(function(str) { return str.trim(); });
-    var ref;
-    for (var i = 0; i < lines.length; i++) {
-      var line = lines[i];
-      var m = line.match(/^Reference\s*=\s*(.*?);$/);
-      if (m) {
-        ref = m[1];
-      }
-      m = line.match(/^IdModule\s*=\s*(.*?);$/);
-      if (m) {
-        var idModule = m[1];
-        if (idModule) {
-          refToModuleName[ref] = idModule.split(/:/)[1];
-        } else {
-          idModule = getKiCadModuleFromRef(ref, data.refsToInventoryId, data.parts);
-          if (idModule) {
-            refToModuleName[ref] = idModule;
-            line = 'IdModule  = kicadlib:' + idModule + ';';
-            lines[i] = line;
-          }
-        }
-      }
-    }
-    fs.writeFile(data.cmpFile, lines.join('\n'));
-    return callback(null, refToModuleName);
-  });
-}
-
-function getKiCadModuleFromRef(ref, refsToInventoryId, parts) {
-  var inventoryId = refsToInventoryId[ref];
-  if (!inventoryId) {
-    console.log("Could not find inventory item for: " + ref);
-    return null;
-  }
-  var part = parts[inventoryId];
-  if (!part) {
-    console.log("Could not find part for: " + ref + " (inventory id: " + inventoryId + ")");
-    return null;
-  }
-  if (!part.kicadModules || part.kicadModules.length == 0) {
-    console.log("Could not find kicadModules for: " + ref + " (inventory id: " + inventoryId + ")");
-    return null;
-  }
-  if (part.kicadModules.length != 1) {
-    console.log("Wrong number of kicadModules for: " + ref + " (inventory id: " + inventoryId + "): " + part.kicadModules);
-    return null;
-  }
-  return part.kicadModules[0].name;
-}
-
 function copyKiCadLibraryModule(callback, data) {
-  var moduleNames = hashValuesToSet(data.refToModuleName);
-  return async.forEachLimit(moduleNames, 5, function(moduleName, callback) {
+  var parts = data.parts;
+  var kicadModules = getKicadModulesFromParts(parts);
+  return async.forEachLimit(kicadModules, 5, function(moduleName, callback) {
     var localModPath = path.resolve('mods', moduleName + '.kicad_mod');
     return fs.exists(localModPath, function(exists) {
       if (exists) {
@@ -179,9 +128,70 @@ function copyKiCadLibraryModule(callback, data) {
   }, callback);
 }
 
+function getKicadModulesFromParts(parts) {
+  var results = {};
+  Object.keys(parts).forEach(function(partName) {
+    var part = parts[partName];
+    part.kicadModules.forEach(function(kicadModule) {
+      results[kicadModule.name] = true;
+    });
+  });
+  return Object.keys(results);
+}
+
+function associate(callback, data) {
+  var schFiles = data.schFiles;
+  var parts = data.parts;
+  async.forEach(schFiles, function(schFile, callback) {
+    var lines = schFile.lines;
+    var ref = null;
+    for (var i = 0; i < lines.length; i++) {
+      var line = lines[i].trim();i
+            
+      var m = line.match(/^L (.*?) (.*?)$/);
+      if (m) {
+        ref = m[2];
+      }
+
+      m = line.match(/^F (.*?) ".*?" (.*)$/);
+      if (m) {
+        if(parseInt(m[1]) == 2) {
+          var inventoryId = schFile.refsToInventoryId[ref];
+          var associatedModule = schFile.refToModule[ref];
+          if(!associatedModule && inventoryId) {
+            var part = parts[inventoryId];
+            if(part) {
+              var kicadModules = part.kicadModules;
+              if(kicadModules.length == 1) {
+                var kicadModule = kicadModules[0].name;
+                lines[i] = 'F 2 "kicadlib:' + kicadModule + '" ' + m[2];
+              } else if(kicadModules.length == 0) {
+                console.log('no module for ' + ref + ' on inventory id ' + inventoryId);
+              } else {
+                console.log('too many modules for ' + ref + ' on inventory id ' + inventoryId);
+              }
+            }
+          }
+        }
+      }
+    }
+    return callback();
+  }, callback);
+}
+
+function writeSchFiles(callback, data) {
+  var schFiles = data.schFiles;
+  async.forEach(schFiles, function(schFile, callback) {
+    var fileName = schFile.schFile;
+    var data = schFile.lines.join('\n');
+    return fs.writeFile(fileName, data, callback);
+  }, callback);
+}
+
 function copyFile(source, target, callback) {
   var cbCalled = false;
 
+  console.log('copying ' + source + ' -> ' + target);
   var rd = fs.createReadStream(source);
   rd.on("error", function(err) {
     done(err);
@@ -208,6 +218,14 @@ function hashValuesToSet(hash) {
   Object.keys(hash).forEach(function(key) {
     var val = hash[key];
     results[val] = true;
+  });
+  return Object.keys(results);
+}
+
+function toSet(arr) {
+  var results = {};
+  arr.forEach(function(e) {
+    results[e] = true;
   });
   return Object.keys(results);
 }
